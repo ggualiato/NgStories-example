c2be782f488a96552f932cb87222e47da5b1896c
Giovanni Gualiato <ggualiato@gmail.com>
2018-08-27 23:38:48 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  itens = [
+    { img: 'http://placekitten.com/200/350', nome: 'Giovanni' },
+    { img: 'http://placekitten.com/300/380', nome: 'Diogo' },
+    { img: 'http://placekitten.com/400/300', nome: 'Farias' },
+    { img: 'http://placekitten.com/400/500', nome: 'Pacheco' },
+    { img: 'http://placekitten.com/350/500', nome: 'Diego' },
+    { img: 'http://placekitten.com/200/200', nome: 'Gualiato' },
+    { img: 'http://placekitten.com/200/300', nome: 'Lucas' },
+    { img: 'http://placekitten.com/200/300', nome: 'Matheus' },
+    { img: 'http://placekitten.com/200/300', nome: 'Gustavo' },
+    { img: 'http://placekitten.com/200/300', nome: 'Pachequilha' },
+    { img: 'http://placekitten.com/300/300', nome: 'Rato' },
+    { img: 'http://placekitten.com/200/300', nome: 'Jamal' },
+    { img: 'http://placekitten.com/208/300', nome: 'Giovanni' },
+    { img: 'http://placekitten.com/200/300', nome: 'Giovanni' },
+    { img: 'http://placekitten.com/200/300', nome: 'Giovanni' },
+    { img: 'http://placekitten.com/290/300', nome: 'Giovanni' }
+  ]
+
+  selectedItem: Object
+
+  selectItem(item: Object): void {
+    if (this.selectedItem == item) {
+      this.selectedItem = {
+        
+      }
+    } else {
+      this.selectedItem = item
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { StoriesItemComponent } from './stories-item/stories-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StoriesItemComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule
   ],
